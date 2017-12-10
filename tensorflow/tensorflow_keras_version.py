dd0fe1c082172150cbd53ed75879b85d4d884295
wlunareve <wlunareve@gmail.com>
2017-12-10 23:20:33 +0800

    tensorflow and keras install and version test

---
+++ tensorflow/tensorflow_keras_version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 00:10:26 2017
+
+@author: wlunareve
+"""
+
+import tensorflow as tf
+import keras
+
+print(tf.__version__)
+print(keras.__version__)
