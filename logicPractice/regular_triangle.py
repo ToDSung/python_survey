658648d1ac7ed301997a528d77a82e317be67c98
wlunareve <wlunareve@gmail.com>
2017-12-15 00:32:48 +0800

    input  a integer number ，ouput a number of layers regular triangle

--- logicPractice/regular_triangle.py
+++ logicPractice/regular_triangle.py
@@ -7,7 +7,7 @@ Created on Thu Dec 14 22:41:09 2017
 #try to create regular triangle
 def regularTriangleByMyself(number):
     for i in range(1,number):
-        if i%2==1:
+        if i%2==1: #忽視偶數行生成
             for x in range (1,number-int(i/2)):
                 print (' ',end='')
             for j in range(0,i):
@@ -20,9 +20,21 @@ def judgeInput(number):
         return number+1
     else:
         return number
+"""
 regularTriangleByMyself(judgeInput(1))
 regularTriangleByMyself(judgeInput(2))
 regularTriangleByMyself(judgeInput(3))
 regularTriangleByMyself(judgeInput(4))
 regularTriangleByMyself(judgeInput(5))
+"""
+#輸入數字出現指定層數的等腰三角形
 
+def createRegularTriangle(number):
+    for i in range(1,number):
+        for x in range (1,number-i):
+            print(' ',end='')
+        for j in range (1,i*2):
+            print('+',end='')
+        print()
+        
+createRegularTriangle(10)
