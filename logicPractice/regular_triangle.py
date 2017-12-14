bd571f8467387d98d463e64ac96606b4937d2dd1
wlunareve <wlunareve@gmail.com>
2017-12-15 00:41:34 +0800

    change variable name

--- logicPractice/regular_triangle.py
+++ logicPractice/regular_triangle.py
@@ -6,11 +6,11 @@ Created on Thu Dec 14 22:41:09 2017
 """
 #try to create regular triangle
 def regularTriangleByMyself(number):
-    for i in range(1,number):
-        if i%2==1: #忽視偶數行生成
-            for x in range (1,number-int(i/2)):
+    for layer in range(1,number):
+        if layer%2==1: #忽視偶數行生成
+            for blank in range (1,number-int(layer/2)):
                 print (' ',end='')
-            for j in range(0,i):
+            for stars in range(0,layer):
                 print('+',end='')
             print()
     print()            
@@ -30,10 +30,10 @@ regularTriangleByMyself(judgeInput(5))
 #輸入數字出現指定層數的等腰三角形
 
 def createRegularTriangle(number):
-    for i in range(1,number):
-        for x in range (1,number-i):
+    for layer in range(1,number):
+        for blank in range (1,number-layer):
             print(' ',end='')
-        for j in range (1,i*2):
+        for stars in range (1,layer*2):
             print('+',end='')
         print()
         
