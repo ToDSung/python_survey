2618027707574e9b41a7ad0fee48f5a74969f593
wlunareve <wlunareve@gmail.com>
2017-12-25 20:31:07 +0800

    change name

--- Pandas/pandas_select.py
+++ Pandas/pandas_select.py
@@ -109,16 +109,3 @@ print(finance.loc[finance['Volume']>50000000,['Date','Volume']])
 157  2017-08-17  56840300
 '''
 
-'''更新多個欄位
-# 指派一個值會讓索引到的每一個值，都被指派為新值
-df.loc[df['Sex']=='male', 'Sex'] = 1
-df.loc[df['Sex']=='female', 'Sex'] = 0
-
-# 指派一個list
-std = df['Age'].std()  #算出標準差
-mean = df['Age'].mean()  #算出平均數
-size = len(df[pd.isnull(df['Age'])])  #算出null值的長度
-age_null_random_list = np.random.randint(mean - std, mean + std, size=size)  #產生一個屆在一個標準差之內的隨機整數
-df.loc[pd.isnull(df['Age']), 'Age'] = age_null_random_list  # 將隨機整數指派給null值
-'''
-
