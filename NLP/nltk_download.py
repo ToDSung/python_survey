986f9ffb729360e17b9b97ff2ea4c7a6f59cbb8b
wlunareve <wlunareve@gmail.com>
2017-12-26 00:56:45 +0800

    nltk downloads package syntax

---
+++ NLP/nltk_download.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 23:35:43 2017
+
+@author: wlunareve
+"""
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
