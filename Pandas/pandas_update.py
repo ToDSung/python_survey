d7f78ca4d727601f39eefe7f69c64304d31f4684
wlunareve <wlunareve@gmail.com>
2017-12-25 23:04:49 +0800

    try to update data and std() mean() function use

--- Pandas/pandas_update.py
+++ Pandas/pandas_update.py
@@ -24,3 +24,27 @@ size = len(df[pd.isnull(df['Age'])])  #算出null值的長度
 age_null_random_list = np.random.randint(mean - std, mean + std, size=size)  #產生一個屆在一個標準差之內的隨機整數
 df.loc[pd.isnull(df['Age']), 'Age'] = age_null_random_list  # 將隨機整數指派給null值
 '''
+
+print(finance.loc[len(finance)-1])
+'''
+Open              174.6
+'''
+
+'''
+選出指定的值重新指派
+'''
+finance.loc[len(finance)-1,'Open'] = 152
+print(finance.loc[len(finance)-1])
+'''
+Open                152
+'''
+#std Standard deviation 標準差
+print(finance['Open'].std())
+'''
+32.03776559931819
+'''
+#mean mean value 平均值
+print(finance['Open'].mean())
+'''
+140.65803729752068
+'''
