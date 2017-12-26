af9e67289cbfcf1aa95bc6a51311f97375d24227
wlunareve <wlunareve@gmail.com>
2017-12-27 00:34:05 +0800

    small change

--- tensorflow/tensorflow_keras_version.py
+++ tensorflow/tensorflow_keras_version.py
@@ -8,7 +8,7 @@ Created on Sun Dec 10 00:10:26 2017
 import tensorflow as tf
 #import keras
 
-print(tf.__version__)
+#print(tf.__version__)
 #print(keras.__version__)
 
 hw =tf.constant("Hello World!")
