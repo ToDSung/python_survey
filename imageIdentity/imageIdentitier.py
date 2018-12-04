054f188b75b7a3273efc75a58a75b2940c3aa3c4
wlunareve <wlunareve@gmail.com>
2018-12-04 22:46:49 +0800

    Fix image file name

--- imageIdentity/imageIdentitier.py
+++ imageIdentity/imageIdentitier.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = 'E://Program Files (x86)/Tesseract-OCR/tesseract.exe'
 
-image = Image.open(r".\Hello World.png")
+image = Image.open(r".\HelloWorld.png")
 text = pytesseract.image_to_string(image)
 print(text)
