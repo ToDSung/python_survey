59d645d86071ae9d610032e5713c021658d1f1c6
wlunareve <wlunareve@gmail.com>
2018-12-07 14:20:11 +0800

    Finished runoob python library re

--- regex/regex.py
+++ regex/regex.py
@@ -36,13 +36,31 @@ else:
     print('No mathch')
 print(eg5.group())
 
-# #.*$ 用來比對python  註解 eg: 2004-959-559 # 這是一個國外電話號碼
+# #.*$ 用來比對python 註解 eg: 2004-959-559 # 這是一個國外電話號碼
 
 eg6 = re.search(r'#.*$', '2004-959-559 # 這是一個國外電話號碼', re.M|re.I)
 print(eg6.group())
 # # 這是一個國外電話號碼
 eg7 = re.sub(r'#.*$', "", '2004-959-559 # 這是一個國外電話號碼')
 print(eg7)
-#2004-959-559
+# 2004-959-559
+# \D = [^0-9] 比對任何非數字字符出來 替換成 ""
 eg8 = re.sub(r'\D', "", '2004-959-559 # 這是一個國外電話號碼')
 print(eg8)
+# 2004959559
+
+pattern = re.compile(r'\d+')
+
+eg9 = pattern.findall('www.123.com.tw.546')
+eg10 = pattern.findall('91huioph401h07123')
+
+print(eg9)
+# ['123', '546']
+print(eg10)
+# ['91', '401', '07123']
+
+# '(?P...)'
+s = '1102231990xxxxxxxx'
+res = re.search(r'(?P<province>\d{3})(?P<city>\d{3})(?P<born_year>\d{4})',s)
+print(res.groupdict())
+# {'province': '110', 'city': '223', 'born_year': '1990'}
